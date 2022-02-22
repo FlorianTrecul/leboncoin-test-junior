3b98c8aea4b98a69b2c1c6b5afb5c078916349ac
Florian TRECUL <trecul.florian@gmail.com>
2022-02-22 20:58:11 +0100

    Setup navigation, dependency injection and base fragment

---
+++ app/src/main/java/com/floriantrecul/leboncointestjunior/presentation/albumlist/AlbumListFragment.kt
@@ -0,0 +1,24 @@
+package com.floriantrecul.leboncointestjunior.presentation.albumlist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.floriantrecul.leboncointestjunior.databinding.FragmentAlbumListBinding
+import com.floriantrecul.leboncointestjunior.presentation.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AlbumListFragment : BaseFragment<FragmentAlbumListBinding, AlbumListViewModel>() {
+
+    override val viewModel: AlbumListViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?) =
+        FragmentAlbumListBinding.inflate(inflater, container, false)
+
+}
